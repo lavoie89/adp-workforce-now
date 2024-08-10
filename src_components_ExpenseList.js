f8384be897627665cd74b837f5e4ddb399c1e6a2
lavoie89 <mauricio.lavoie89@outlook.com>
2024-08-10 10:34:20 +0530

    Create src_components_ExpenseList.js

---
+++ src_components_ExpenseList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ExpenseList.css';
+
+function ExpenseList() {
+  return (
+    <div className="ExpenseList">
+      <h2>Expense List</h2>
+      {/* List of expenses go here */}
+    </div>
+  );
+}
+
+export default ExpenseList;
