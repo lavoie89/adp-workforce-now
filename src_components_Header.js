2d5688c2d154690ce2c1b1f6bcdbf4b075fd4017
lavoie89 <mauricio.lavoie89@outlook.com>
2024-08-10 10:35:08 +0530

    Create src_components_Header.js

---
+++ src_components_Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>ADP Workforce Now</h1>
+    </div>
+  );
+}
+
+export default Header;
